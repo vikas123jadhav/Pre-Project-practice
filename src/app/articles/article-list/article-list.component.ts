ffb965dda08202afaa75cdd899116544f09d9cb8
VikasNandakumarJadhav <vikasjadhav4569@gmail.com>
2023-01-19 13:36:33 +0530

    created all required modules

---
+++ src/app/articles/article-list/article-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-list',
+  templateUrl: './article-list.component.html',
+  styleUrls: ['./article-list.component.scss']
+})
+export class ArticleListComponent {
+
+}
---
+++ src/app/articles/featured-articles/featured-articles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-articles',
+  templateUrl: './featured-articles.component.html',
+  styleUrls: ['./featured-articles.component.scss']
+})
+export class FeaturedArticlesComponent {
+
+}
